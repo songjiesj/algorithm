556ac33ab45c52b18a1b270aa1e60dc369e6d7d7
songjiesj <920459930@qq.com>
2020-05-05 21:45:52 +0800

    'another test-example'

--- topo-sort/topo-sort-dfs.cpp
+++ topo-sort/topo-sort-dfs.cpp
@@ -74,20 +74,32 @@ void Graph::topologicalSort()
     { 
         cout << Stack.top() << " "; 
         Stack.pop(); 
-    } 
+    }
+    cout << endl; 
 } 
   
 // Driver program to test above functions 
 int main() 
 { 
     // Create a graph given in the above diagram 
-    Graph g(6); 
-    g.addEdge(5, 2); 
-    g.addEdge(5, 0); 
-    g.addEdge(4, 0); 
-    g.addEdge(4, 1); 
-    g.addEdge(2, 3); 
-    g.addEdge(3, 1); 
+    // Graph g(6); 
+    // g.addEdge(5, 2); 
+    // g.addEdge(5, 0); 
+    // g.addEdge(4, 0); 
+    // g.addEdge(4, 1); 
+    // g.addEdge(2, 3); 
+    // g.addEdge(3, 1); 
+
+    Graph g(5);
+	g.addEdge(1, 0);
+	g.addEdge(2, 0);
+	g.addEdge(3, 0);
+	g.addEdge(4, 0);
+	g.addEdge(2, 1);
+	g.addEdge(3, 1);
+	g.addEdge(4, 1);
+	g.addEdge(2, 3);
+	g.addEdge(4, 3);
   
     cout << "Following is a Topological Sort of the given graph \n"; 
     g.topologicalSort(); 
